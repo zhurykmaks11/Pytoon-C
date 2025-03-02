9f5d9add22be340af525be394663d766b3bdd1f0
zhurykmaks11 <zhurik.max@gmail.com>
2025-03-02 23:35:48 +0200

    add 4 task

---
+++ c#/c_lab2/lab2_4/ConsoleApp1/task4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class JaggedArrayTask
+{
+    static void Main()
+    {
+        Random random = new Random();
+
+        // Введення кількості рядків
+        Console.Write("Введіть кількість рядків (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Створення зубчастого масиву
+        int[][] jaggedArray = new int[n][];
+        
+        // Заповнення масиву випадковими числами та виведення його
+        Console.WriteLine("\nЗгенерований зубчастий масив:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Введіть кількість елементів у рядку {i + 1}: ");
+            int m = int.Parse(Console.ReadLine());
+            jaggedArray[i] = new int[m];
+
+            for (int j = 0; j < m; j++)
+            {
+                jaggedArray[i][j] = random.Next(-50, 50); // Числа в діапазоні -50…49
+            }
+
+            Console.WriteLine($"Рядок {i + 1}: " + string.Join(" ", jaggedArray[i]));
+        }
+
+        // Введення порогового значення
+        Console.Write("\nВведіть порогове значення: ");
+        int threshold = int.Parse(Console.ReadLine());
+
+        // Масив для збереження кількості елементів, більших за порогове значення
+        int[] countArray = new int[n];
+
+        // Підрахунок кількості елементів, що перевищують поріг
+        for (int i = 0; i < n; i++)
+        {
+            int count = 0;
+            foreach (int num in jaggedArray[i])
+            {
+                if (num > threshold)
+                    count++;
+            }
+            countArray[i] = count;
+        }
+
+        // Вивід результату
+        Console.WriteLine("\nКількість елементів, більших за " + threshold + ", у кожному рядку:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine($"Рядок {i + 1}: {countArray[i]} елементів");
+        }
+    }
+}
