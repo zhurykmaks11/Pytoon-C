bbef3597a5123ef932d996829909a01c27a43983
zhurykmaks11 <zhurik.max@gmail.com>
2025-04-07 16:18:59 +0300

    add lab7 task 2

---
+++ c#/c_lab7/WpfApp2/WpfApp2/Models/Visit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WpfApp2.Models
+{
+    public struct Visit
+    {
+        public string Page { get; set; }
+        public string IP { get; set; }
+        public string Browser { get; set; }
+        public DateTime Date { get; set; }  
+
+        public string GetDayOfWeek()
+        {
+            return ((int)Date.DayOfWeek == 0 ? 7 : (int)Date.DayOfWeek).ToString();
+        }
+
+        public override string ToString()
+        {
+            return $"Сторінка: {Page} | IP: {IP} | Браузер: {Browser} | {Date:ddMMyyyy} " +
+                   $"{Date:HH:mm:ss} | День: {GetDayOfWeek()}";
+        }
+    }
+}
